8dc20f53d095b1d04f8458163e2b504ee934bf4b
lmaposadas <112681437+lmaposadas@users.noreply.github.com>
2023-08-31 00:45:43 +0800

    Update Cs4TruthPosadas.java

--- src/cs4truthposadas/Cs4TruthPosadas.java
+++ src/cs4truthposadas/Cs4TruthPosadas.java
@@ -16,8 +16,43 @@ public class Cs4TruthPosadas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String greeting = "Hello World!";
-        System.out.println(greeting + " Hello Pisay!");
+        String group1 = "\nSEVENTEEN";
+        int debut1 = 2015;
+        int member1 = 13;
+        String ent1 = "PLEDIS";
+        String group2 = "\nNCT 127";
+        int debut2 = 2016;
+        int member2 = 10;
+        String ent2 = "SM";
+        String group3 = "\nMONSTA X";
+        int debut3 = 2015;
+        int member3 = 6;
+        String ent3 = "Starship";
+        int totalmembers = member1 + member2 + member3;
+        int diff = member1 - member3;
+        String ans1 = "null";
+        String ans2 = "null";
+    
+    System.out.println(group1 + "\nNo. of members: " + member1 + "\nEntertainment Company: " + ent1 + "\nYear of Debut: " + debut1); 
+    System.out.println(group2 + "\nNo. of members: " + member2 + "\nEntertainment Company: " + ent2 + "\nYear of Debut: " + debut2);
+    System.out.println(group3 + "\nNo. of members: " + member3 + "\nEntertainment Company: " + ent3 + "\nYear of Debut: " + debut3);
+
+    System.out.println("\nTotal number of members: " + totalmembers);
+
+    if (debut1 < debut2 || debut3 < debut2) {
+      ans1 = "true"; }
+    else {
+      ans1 = "false"; }
+    System.out.println("NCT 127 debuted later than SEVENTEEN and MONSTA X: " + ans1);
+    
+    if (ent1 == ent2) {
+      ans2 = "true"; }
+    else {
+      ans2 = "false"; }
+        
+    System.out.println("SEVENTEEN and NCT 127 come from the same entertainment company: " + ans2);
+
+    System.out.println("Difference between the number of MONSTA X and SEVENTEEN members: " + diff);
     }
     
 }
